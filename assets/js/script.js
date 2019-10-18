24ae92315c65b33f1f330370d10a8b4fc884c6f1
Chris Neal <voodoodaddy2000@gmail.com>
2019-10-18 08:04:08 -0700

    Timer installed and selecting a question - the order is random

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var $headerRow = document.getElementById("header-row");
 var $headerCol = document.getElementById("header-col");
 var $answerRow = document.getElementById("answer-row");
 var $answerCol = document.getElementById("answer-col");
+var $timerDiv = document.getElementById("timerDiv");
 var $mainHeading = document.getElementById("main-heading");
 var $question = document.getElementById("question");
 var $startButton = document.getElementById("start-btn");
@@ -10,9 +11,12 @@ var $highScoreButton = document.getElementById("high-score-btn");
 var $instructions = document.getElementById("instructions");
 var $hrDivider = document.getElementById("hr-divider");
 var questionCounter;
+var currentTimer;
+var randomQuestion = [];
 
 function setupMainPage() {
     $headerRow.setAttribute("class", "row header-row-m align-items-center");
+    $timerDiv.setAttribute("class", "timerDiv d-none");
     $hrDivider.setAttribute("class", "d-none");
     $answerRow.setAttribute("class", "d-none");
     $headerCol.appendChild($mainHeading);
@@ -25,6 +29,7 @@ function setupMainPage() {
 
 function setupQuestionsPage() {
     $headerRow.setAttribute("class", "row header-row-q align-items-center");
+    $timerDiv.setAttribute("class", "timerDiv");
     $hrDivider.setAttribute("class", "row");
     $answerRow.setAttribute("class", "row  answer-row");
     $headerCol.appendChild($question);
@@ -33,13 +38,27 @@ function setupQuestionsPage() {
     $startButton.parentNode.removeChild($startButton);
     $highScoreButton.parentNode.removeChild($highScoreButton);
     questionCounter = questions.length;
-    console.log(questionCounter);
-    // for (i = 0; i < questionCounter; i++) {
+    for (i = 0; i < questionCounter; i++) {
+        randomQuestion[i] = i;
+    }
+    currentTimer = 15 * questionCounter;
+    $timerDiv.children[0].innerHTML = currentTimer;
+    var timerInterval = setInterval(function() {
+        currentTimer--;
+        $timerDiv.children[0].innerHTML = currentTimer;
+    
+        if(currentTimer == 0) {
+          clearInterval(timerInterval);
+        }
+    
+      }, 1000);
+    
     runQuestion();
-    // }
 }
 
 function runQuestion() {
+    var currentQuestion = randomQuestion.splice(Math.floor(Math.random() * randomQuestion.length), 1);
+    $question.innerHTML = questions[currentQuestion].title;
     for (i = 0; i < 4; i++) {
         var answerRow = document.createElement('div');
         var answerCol = document.createElement('div');
@@ -47,12 +66,20 @@ function runQuestion() {
         var answerBtn = document.createElement('button');
         answerBtn.setAttribute("type", "button");
         answerBtn.setAttribute("class", "btn");
-        answerBtn.setAttribute("id","answer" + i);
-        answerBtn.innerText = "answer" + i;
+        answerBtn.setAttribute("id", "answer" + i);
+        answerBtn.innerText = questions[currentQuestion].choices[i];
         answerCol.appendChild(answerBtn);
         $answerCol.appendChild(answerRow);
         document.getElementById("answer" + i).addEventListener("click", setupMainPage);
     }
+}
+
+function checkAnswer(){
+    //do something
+}
+
+function endQuiz(){
+    //do something
 }
 
 //Event Listeners
