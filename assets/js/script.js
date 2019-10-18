023fe58ec7d62fdd58133fed93416a799c26e590
Chris Neal <voodoodaddy2000@gmail.com>
2019-10-17 22:08:19 -0700

    Main design layedout and basic functionality

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+var $mainContainer = document.getElementById("main-container");
+var $headerRow = document.getElementById("header-row");
+var $headerCol = document.getElementById("header-col");
+var $answerRow = document.getElementById("answer-row");
+var $answerCol = document.getElementById("answer-col");
+var $mainHeading = document.getElementById("main-heading");
+var $question = document.getElementById("question");
+var $startButton = document.getElementById("start-btn");
+var $highScoreButton = document.getElementById("high-score-btn");
+var $instructions = document.getElementById("instructions");
+var $hrDivider = document.getElementById("hr-divider");
+var questionCounter;
+
+function setupMainPage() {
+    $headerRow.setAttribute("class", "row header-row-m align-items-center");
+    $hrDivider.setAttribute("class", "d-none");
+    $answerRow.setAttribute("class", "d-none");
+    $headerCol.appendChild($mainHeading);
+    $headerCol.appendChild($instructions);
+    $headerCol.appendChild($startButton);
+    $headerCol.appendChild($highScoreButton);
+    $question.parentNode.removeChild($question);
+    $answerCol.innerHTML = '';
+}
+
+function setupQuestionsPage() {
+    $headerRow.setAttribute("class", "row header-row-q align-items-center");
+    $hrDivider.setAttribute("class", "row");
+    $answerRow.setAttribute("class", "row  answer-row");
+    $headerCol.appendChild($question);
+    $mainHeading.parentNode.removeChild($mainHeading);
+    $instructions.parentNode.removeChild($instructions);
+    $startButton.parentNode.removeChild($startButton);
+    $highScoreButton.parentNode.removeChild($highScoreButton);
+    questionCounter = questions.length;
+    console.log(questionCounter);
+    // for (i = 0; i < questionCounter; i++) {
+    runQuestion();
+    // }
+}
+
+function runQuestion() {
+    for (i = 0; i < 4; i++) {
+        var answerRow = document.createElement('div');
+        var answerCol = document.createElement('div');
+        answerRow.appendChild(answerCol);
+        var answerBtn = document.createElement('button');
+        answerBtn.setAttribute("type", "button");
+        answerBtn.setAttribute("class", "btn");
+        answerBtn.setAttribute("id","answer" + i);
+        answerBtn.innerText = "answer" + i;
+        answerCol.appendChild(answerBtn);
+        $answerCol.appendChild(answerRow);
+        document.getElementById("answer" + i).addEventListener("click", setupMainPage);
+    }
+}
+
+//Event Listeners
+window.addEventListener("DOMContentLoaded", setupMainPage);
+$startButton.addEventListener("click", setupQuestionsPage);
